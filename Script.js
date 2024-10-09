a31bcd663d7bc4d72c98d07e69be24a869e89f18
yashsoni2191 <yashsoni2191@gmail.com>
2024-10-09 15:46:37 +0530

    Weather-API

--- Script.js
+++ Script.js
@@ -1,16 +1,26 @@
-document.getElementById("fetch-weather").onclick = async () => {
-    const city = document.getElementById("city-input").value
-    const apiKey = "328b33f675347921285d1ad80f057964"
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-    const data = await response.json()
-
+async function fetchWeather(city) {
+    const apiKey = "328b33f675347921285d1ad80f057964";
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const data = await response.json();
+  
     if (data.weather) {
-        document.getElementById("city-name").textContent = data.name
-        document.getElementById("temp").textContent = `${data.main.temp} °C`
-        document.getElementById("description").textContent = data.weather[0].description
-        document.getElementById("weather-display").classList.add("visible")
-
-        const weatherType = data.weather[0].main.toLowerCase()
-        document.body.className = weatherType
+      document.getElementById("city-name").textContent = data.name;
+      document.getElementById("temp").textContent = `${data.main.temp} °C`;
+      document.getElementById("description").textContent = data.weather[0].description;
+      document.getElementById("weather-display").classList.add("visible");
+  
+      const weatherType = data.weather[0].main.toLowerCase();
+      document.body.className = weatherType;
     }
-}
+  }
+
+  document.getElementById("fetch-weather").onclick = () => {
+    const city = document.getElementById("city-input").value;
+    fetchWeather(city);
+  };
+
+  window.onload = () => {
+    const defaultCity = "Rajkot"; 
+    fetchWeather(defaultCity);
+  };
+  
